70fd2c21df6ddba08d04eed271606a21d88e18f3
biennguyen1108 <115470524+biennguyen1108@users.noreply.github.com>
2023-01-31 11:25:27 +0700

    Add files via upload

---
+++ ifelse.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+
+$mong = 2;
+if($mong==1){
+    echo"đi thăm bà con";
+}
+else if($mong==2){
+    echo"đi nhậu";
+}
+else if($mong==3){
+    echo"đi đánh bài";
+}
+else if($mong==4){
+    echo"đi chùa";
+}
+else if($mong==5){
+    echo"đi chơi";
+}
+else if($mong==6){
+    echo"đi học";
+}
+
+?>
+
+</body>
+</html>
---
+++ switch.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$mong = "mot";
+
+switch ($mong) {
+  case "mot":
+    echo "đi chúc tết bà con";
+    break;
+  case "hai":
+    echo "đi nhậu với bạn";
+    break;
+  case "ba":
+    echo "đi đánh bài";
+    break;
+  default:
+    echo "ở nhà ngủ";
+}
+?>
+</body>
+</html>
